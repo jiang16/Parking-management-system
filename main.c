2a5f506e3ccca5d47e158989b0122855c3702c05
jiang16 <1796362795@qq.com>
2018-01-11 08:27:47 +0800

    Update main.c

--- main.c
+++ main.c
@@ -6,15 +6,18 @@
     > Email: 1796362795@qq.com 
  **********************************************************************/
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include "Parking.h"
 
-const int N = 10;                 //停车场的容量，默认为10
+#define N 5                       //停车场的容量，默认为5
 
 int main(int argv, char *argc[]){
-    Init(N);
-    PrintWelcome();   
+    Init(N);       
     while(1)
     {
+        system("clear");          //清屏
+        PrintWelcome();
         printf("● 请在此输入操作所对应的数字：\n");
         char num[30];             //接收输入的操作数字
         scanf("%s", num);
@@ -28,14 +31,15 @@ int main(int argv, char *argc[]){
             scanf("%s", id);
             int res = Judge(id);  //判定结果
             if(res == -1)
-                printf("您输入的车牌号不合法！请确保输入的车牌号为非负整数\n"); 
+                printf("▲ 您输入的车牌号不合法！请确保输入的车牌号为非负整数\n"); 
             else if(opNum == 1)
                 Park(res);
             else if(opNum = 2)
                 PickUp(res);     
         }
         else
-            printf("没有对应的操作！请重新输入\n");
+            printf("▲ 没有对应的操作！请重新输入\n");
+        sleep(3);                 //睡眠
     }
     Exit();
     return 0;
